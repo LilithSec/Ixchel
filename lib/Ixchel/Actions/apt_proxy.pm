039f7aad943dbcc3001abb987dacad16877a3b8c
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2024-01-21 09:32:26 -0600

    convert apt_proxy to to app base

--- lib/Ixchel/Actions/apt_proxy.pm
+++ lib/Ixchel/Actions/apt_proxy.pm
@@ -4,6 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 use File::Slurp;
+use base 'Ixchel::Actions::base';
 
 =head1 NAME
 
@@ -23,9 +24,13 @@ ixchel -a apt_proxy [B<-w>] [B<-o> <file>]
 
 =head1 CODE SYNOPSIS
 
-    my $filled_in=$ixchel->action(action=>'apt_proxy', opts=>{w=>1});
+    my $results=$ixchel->action(action=>'apt_proxy', opts=>{w=>1});
 
-    print $filled_in;
+    if ($results->{ok}) {
+        print $results->{filled_in};
+    }else{
+        die('Action errored... '.joined("\n", @{$results->{errors}}));
+    }
 
 =head1 DESCRIPTION
 
@@ -39,7 +44,7 @@ The template used is apt_proxy.
 
 =head2 -w
 
-Write out the file instead of stdout.
+Write out the file.
 
 =head2 -o <file>
 
@@ -47,51 +52,20 @@ File to write the out to if -w is specified.
 
 Default :: /etc/apt/apt.conf.d/00aptproxy
 
+=head2 --np
+
+Don't print the the filled in template.
+
+=head1 RESULT HASH REF
+
+    .errors :: A array of errors encountered.
+    .status_text :: A string description of what was done and the results.
+    .ok :: Set to zero if any of the above errored.
+    .filled_in :: The filled in template.
+
 =cut
 
-sub new {
-	my ( $empty, %opts ) = @_;
-
-	my $self = {
-		config => {},
-		vars   => {},
-		arggv  => [],
-		opts   => {},
-	};
-	bless $self;
-
-	if ( defined( $opts{config} ) ) {
-		$self->{config} = $opts{config};
-	}
-
-	if ( defined( $opts{t} ) ) {
-		$self->{t} = $opts{t};
-	} else {
-		die('$opts{t} is undef');
-	}
-
-	if ( defined( $opts{share_dir} ) ) {
-		$self->{share_dir} = $opts{share_dir};
-	}
-
-	if ( defined( $opts{opts} ) ) {
-		$self->{opts} = \%{ $opts{opts} };
-	}
-
-	if ( defined( $opts{argv} ) ) {
-		$self->{argv} = $opts{argv};
-	}
-
-	if ( defined( $opts{vars} ) ) {
-		$self->{vars} = $opts{vars};
-	}
-
-	if ( defined( $opts{ixchel} ) ) {
-		$self->{ixchel} = $opts{ixchel};
-	}
-
-	return $self;
-} ## end sub new
+sub new_extra{}
 
 sub action {
 	my $self = $_[0];
@@ -118,12 +92,21 @@ sub action {
 		);
 	};
 	if ($@) {
-		die( 'Filling in the template failed... ' . $@ );
+		$self->status_add( status => 'Failed to fill out template apt_proxy ... ' . $@, error => 1 );
 	}
+	$self->{results}{filled_in} = $filled_in;
 
 	if ( $self->{opts}{w} ) {
-		write_file( $self->{opts}{o}, $filled_in );
-	} else {
+		eval { write_file( $self->{opts}{o}, $filled_in ); };
+		if ($@) {
+			$self->status_add(
+				status => 'Failed to write out template to "' . $self->{opts}{o} . '" ... ' . $@,
+				error  => 1
+			);
+		}
+	}
+
+	if ( !$self->{opts}{np} ) {
 		print $filled_in;
 	}
 
@@ -138,6 +121,7 @@ sub opts_data {
 	return '
 w
 o=s
+np
 ';
 }
 
