521e82e529e3248540b8fd4f584a3e65da186179
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2024-01-21 21:21:37 -0600

    convert perl_module_via_pkg to action base

--- lib/Ixchel/Actions/perl_module_via_pkg.pm
+++ lib/Ixchel/Actions/perl_module_via_pkg.pm
@@ -3,8 +3,8 @@ package Ixchel::Actions::perl_module_via_pkg;
 use 5.006;
 use strict;
 use warnings;
-use File::Slurp;
 use Ixchel::functions::perl_module_via_pkg;
+use base 'Ixchel::Actions::base';
 
 =head1 NAME
 
@@ -12,11 +12,11 @@ Ixchel::Actions::perl_module_via_pkg - Install Perl modules via the package mana
 
 =head1 VERSION
 
-Version 0.2.0
+Version 0.3.0
 
 =cut
 
-our $VERSION = '0.2.0';
+our $VERSION = '0.3.0';
 
 =head1 CLI SYNOPSIS
 
@@ -24,7 +24,13 @@ ixchel -a perl_module_via_pkg B<--module> <module>
 
 =head1 CODE SYNOPSIS
 
-my $results=$ixchel->action(action=>'perl_module_via_pkg', opts=>{module=>'Monitoring::Sneck'});
+    my $results=$ixchel->action(action=>'perl_module_via_pkg', opts=>{module=>'Monitoring::Sneck'});
+
+    if ($results->{ok}) {
+        print $results->{status_text};
+    }else{
+        die('Action errored... '.joined("\n", @{$results->{errors}}));
+    }
 
 =head1 FLAGS
 
@@ -42,61 +48,17 @@ This must be specified.
 
 =cut
 
-sub new {
-	my ( $empty, %opts ) = @_;
+sub new_extra { }
 
-	my $self = {
-		config => {},
-		vars   => {},
-		arggv  => [],
-		opts   => {},
-	};
-	bless $self;
-
-	if ( defined( $opts{config} ) ) {
-		$self->{config} = $opts{config};
-	}
-
-	if ( defined( $opts{t} ) ) {
-		$self->{t} = $opts{t};
-	} else {
-		die('$opts{t} is undef');
-	}
-
-	if ( defined( $opts{share_dir} ) ) {
-		$self->{share_dir} = $opts{share_dir};
-	}
-
-	if ( defined( $opts{opts} ) ) {
-		$self->{opts} = \%{ $opts{opts} };
-	}
-
-	if ( defined( $opts{argv} ) ) {
-		$self->{argv} = $opts{argv};
-	}
-
-	if ( defined( $opts{vars} ) ) {
-		$self->{vars} = $opts{vars};
-	}
-
-	if ( defined( $opts{ixchel} ) ) {
-		$self->{ixchel} = $opts{ixchel};
-	}
-
-	$self->{results} = {
-		errors      => [],
-		status_text => '',
-		ok          => 0,
-	};
-
-	return $self;
-} ## end sub new
-
-sub action {
+sub action_extra {
 	my $self = $_[0];
 
 	if ( !defined( $self->{opts}{module} ) ) {
-		die('--module is undef');
+		$self->status_add(
+			status => '--module is undef',
+			error  => 1
+		);
+		return undef;
 	}
 
 	$self->status_add( status => 'Installing cpanm via packges' );
@@ -104,24 +66,16 @@ sub action {
 	my $status;
 	eval { $status = perl_module_via_pkg( module => $self->{opts}{module} ); };
 	if ($@) {
-		$self->{results}{status_text} = $self->{results}{status_text} . $@;
 		$self->status_add(
-			status => 'Failed to install ' . $self->{opts}{module} . ' via packages',
+			status => 'Failed to install ' . $self->{opts}{module} . ' via packages' . $@,
 			error  => 1
 		);
 	} else {
-		$self->{results}{status_text} = $self->{results}{status_text} . $status;
 		$self->status_add( status => $self->{opts}{module} . ' installed' );
 	}
 
-	if ( !defined( $self->{results}{errors}[0] ) ) {
-		$self->{results}{ok} = 1;
-	} else {
-		$self->{results}{ok} = 0;
-	}
-
-	return $self->{results};
-} ## end sub action
+	return undef;
+} ## end sub action_extra
 
 sub short {
 	return 'Install a Perl module via packages';
@@ -132,34 +86,5 @@ sub opts_data {
 module=s
 ';
 }
-
-sub status_add {
-	my ( $self, %opts ) = @_;
-
-	if ( !defined( $opts{status} ) ) {
-		return;
-	}
-
-	if ( !defined( $opts{error} ) ) {
-		$opts{error} = 0;
-	}
-
-	if ( !defined( $opts{type} ) ) {
-		$opts{type} = 'perl_module_via_pkg';
-	}
-
-	my ( $sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst ) = localtime(time);
-	my $timestamp = sprintf( "%04d-%02d-%02dT%02d:%02d:%02d", $year + 1900, $mon + 1, $mday, $hour, $min, $sec );
-
-	my $status = '[' . $timestamp . '] [' . $opts{type} . ', ' . $opts{error} . '] ' . $opts{status};
-
-	print $status. "\n";
-
-	$self->{results}{status_text} = $self->{results}{status_text} . $status;
-
-	if ( $opts{error} ) {
-		push( @{ $self->{results}{errors} }, $opts{status} );
-	}
-} ## end sub status_add
 
 1;
