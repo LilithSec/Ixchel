6d902560b527f760c7e0621ddbd422126eaa48f3
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2024-01-21 20:02:35 -0600

    move extend_smart_config over to action base

--- lib/Ixchel/Actions/extend_smart_config.pm
+++ lib/Ixchel/Actions/extend_smart_config.pm
@@ -4,6 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 use File::Slurp;
+use base 'Ixchel::Actions::base';
 
 =head1 NAME
 
@@ -11,11 +12,11 @@ Ixchel::Actions::extend_smart_config - Generates the config for the SMART SNMP e
 
 =head1 VERSION
 
-Version 0.0.1
+Version 0.1.0
 
 =cut
 
-our $VERSION = '0.0.1';
+our $VERSION = '0.1.0';
 
 =head1 CLI SYNOPSIS
 
@@ -23,9 +24,13 @@ ixchel -a extend_smart_config [B<-w>] [B<-o> <file>]
 
 =head1 CODE SYNOPSIS
 
-    my $filled_in=$ixchel->action(action=>'extends_smart_config', opts=>{w=>1});
+    my $results=$ixchel->action(action=>'extends_smart_config', opts=>{w=>1});
 
-    print $filled_in;
+    if ($results->{ok}) {
+        print $results->{filled_in};
+    }else{
+        die('Action errored... '.joined("\n", @{$results->{errors}}));
+    }
 
 =head1 DESCRIPTION
 
@@ -48,53 +53,15 @@ File to write the out to if -w is specified.
 
 Default :: /usr/local/etc/smart-extend.conf
 
+=head2 --np
+
+Don't print the the filled in template.
+
 =cut
 
-sub new {
-	my ( $empty, %opts ) = @_;
+sub new_extra { }
 
-	my $self = {
-		config => {},
-		vars   => {},
-		arggv  => [],
-		opts   => {},
-	};
-	bless $self;
-
-	if ( defined( $opts{config} ) ) {
-		$self->{config} = $opts{config};
-	}
-
-	if ( defined( $opts{t} ) ) {
-		$self->{t} = $opts{t};
-	} else {
-		die('$opts{t} is undef');
-	}
-
-	if ( defined( $opts{share_dir} ) ) {
-		$self->{share_dir} = $opts{share_dir};
-	}
-
-	if ( defined( $opts{opts} ) ) {
-		$self->{opts} = \%{ $opts{opts} };
-	}
-
-	if ( defined( $opts{argv} ) ) {
-		$self->{argv} = $opts{argv};
-	}
-
-	if ( defined( $opts{vars} ) ) {
-		$self->{vars} = $opts{vars};
-	}
-
-	if ( defined( $opts{ixchel} ) ) {
-		$self->{ixchel} = $opts{ixchel};
-	}
-
-	return $self;
-} ## end sub new
-
-sub action {
+sub action_extra {
 	my $self = $_[0];
 
 	# set the default output for -o if not defined
@@ -102,25 +69,39 @@ sub action {
 		$self->{opts}{o} = '/usr/local/etc/smart-extend.conf';
 	}
 
-	# set the default output for -o if not defined
+	# set the default output for -w if not defined
 	if ( !defined( $self->{opts}{w} ) ) {
 		$self->{opts}{w} = 0;
 	}
 
-	my $command=$self->{config}{snmp}{extend_base_dir}.'/smart -g';
-	if ($self->{config}{snmp}{extends}{smart}{additional_update_args}) {
-		$command=$command.' '.$self->{config}{snmp}{extends}{smart}{additional_update_args};
+	my $command = $self->{config}{snmp}{extend_base_dir} . '/smart -g';
+	if ( $self->{config}{snmp}{extends}{smart}{additional_update_args} ) {
+		$command = $command . ' ' . $self->{config}{snmp}{extends}{smart}{additional_update_args};
 	}
-	my $filled_in=`$command 2>&1`;
+	my $filled_in = `$command 2>&1`;
+	if ( $? != 0 ) {
+		$self->status_add( status => '"' . $command . '" exited non-zero... ' . $filled_in, error => 1 );
+		$self->{results}{filled_in} = $filled_in;
+		return undef;
+	}
+	$self->{results}{filled_in} = $filled_in;
 
-	if ( $self->{opts}{w} ) {
-		write_file( $self->{opts}{o}, $filled_in );
-	} else {
+	if ( !$self->{opts}{np} ) {
 		print $filled_in;
 	}
 
-	return $filled_in;
-} ## end sub action
+	if ( $self->{opts}{w} ) {
+		eval { write_file( $self->{opts}{o}, $filled_in ); };
+		if ($@) {
+			$self->status_add(
+				status => 'Failed to write out filled in template to "' . $self->{opts}{o} . '" ... ' . $@,
+				error  => 1
+			);
+		}
+	}
+
+	return undef;
+} ## end sub action_extra
 
 sub short {
 	return 'Generates the config for the SMART SNMP extend.';
@@ -130,6 +111,7 @@ sub opts_data {
 	return '
 w
 o=s
+np
 ';
 }
 
